b8097d3bf17bbf0765d25f96b2591d07a0e0d879
Fred Kelly <me@fredkelly.net>
2012-04-05 18:55:26 +0100

    init

---
+++ nodistract.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Blocks & unblocks distracting domains using hosts file.
+# Written by Fred Kelly <me@fredkelly.net> 5/4/2012.
+
+HOSTS="/etc/hosts"
+CURRENT=$(cat $HOSTS)
+EMPTIED=$(sed "/## BEGIN BLOCKING DISTRACTIONS ##/,/## END BLOCKING DISTRACTIONS ##/d" $HOSTS)
+
+if [ "$CURRENT" == "$EMPTIED" ]; then
+	# add lines to hosts
+	(
+		echo -e "\n## BEGIN BLOCKING DISTRACTIONS ##"
+		echo -e "127.0.0.1\tfacebook.com"
+		echo -e "127.0.0.1\ttwitter.com"
+		# add more here...
+		echo -e "## BEGIN BLOCKING DISTRACTIONS ##\n"
+	) >> /etc/hosts
+else
+	# remove lines from hosts
+	echo "$EMPTIED" > $HOSTS
+fi
+
+
